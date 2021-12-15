7abfe84db370c392954fd3bef2ce9cc206c0714b
Kong Yue Chen <kongyue.chen@gmail.com>
2021-12-15 03:59:20 -0500

    Play feature works with new Discord v13

---
+++ commands/play.js
@@ -0,0 +1,73 @@
+const {joinVoiceChannel,createAudioPlayer,createAudioResource} = require("@discordjs/voice")
+const ytdl = require('ytdl-core');
+const ytSearch = require('yt-search');
+
+
+module.exports = {
+  name: 'play',
+  description: 'Joins and starts playing a video from Youtube',
+  async execute(message, args){
+    const voiceChannel = message.member.voice.channel;
+
+    const noPermissions = "You do not have the correct permissions for this command!";
+    const noArgs = "You need a second arguement!";
+
+    if (!voiceChannel) return message.channel.send('You need to be in a voice channel to use this command');
+    const permissions = voiceChannel.permissionsFor(message.client.user)
+    if (!permissions.has('CONNECT')) return message.channel.send(noPermissions);
+    if (!permissions.has('SPEAK')) return message.channel.send(noPermissions);
+    if (!args.length) return message.channel.send(noArgs);
+
+
+
+
+
+    const videoFinder = async (query) => {
+      const videoResults = await ytSearch(query);
+      return (videoResults.videos.length > 1) ? videoResults.videos[0] : null;
+    };
+
+    const video = await videoFinder(args.join(' '));
+
+
+    const VoiceConnection = joinVoiceChannel({
+      channelId: message.member.voice.channel.id,
+      guildId: message.guild.id,
+      adapterCreator: message.guild.voiceAdapterCreator
+    });
+
+
+    if (video) {
+      const resource = createAudioResource(ytdl(video.url), {
+        filter: 'audioonly',
+        quality: 'highestaudio',
+        inlineVolume: true
+      });
+
+      resource.volume.setVolume(0.2);
+      const player = createAudioPlayer();
+      VoiceConnection.subscribe(player);
+      player.play(resource)
+
+      // player.on("idle", async() => {
+      //   try {
+      //     player.stop()
+      //   } catch (error) {}
+
+      //   try {
+      //     VoiceConnection.destroy()
+      //   } catch (error) {}
+
+      //   await new Promise(res => setTimeout(() => res(2), 500))
+      //   joinChannel(channelId);
+
+
+      // })
+
+      await message.reply(`:thumbs up: Now playing ***${video.title}***`);
+    } else {
+      message.channel.send('No videos results found.')
+    }
+
+  }
+}
---
+++ index.js
@@ -0,0 +1,43 @@
+const dotenv = require('dotenv').config();
+const TOKEN = process.env.TOKEN || TestToken;
+const prefix = '-';
+const { Client, Collection, Intents } = require('discord.js');
+const fs = require('fs');
+
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_VOICE_STATES
+  ]
+});
+
+client.commands = new Collection();
+
+// Checks every file in commands file and set it's name as the command key
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
+client.once('ready', () => {
+  console.log('GroovyRhythm is online!');
+})
+
+// Responds to commands after ridding the prefix
+client.on('messageCreate', (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (command === 'ping') {
+    client.commands.get(command).execute(message, args);
+  } else if (command === 'play') {
+    client.commands.get(command).execute(message, args);
+  }
+})
+
+//Logins with token
+client.login(TOKEN);
